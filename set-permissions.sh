06ba645c0eea80e7f3b2aaadac63527f46d8f793
Sunny <sunnynbhatt@gmail.com>
2019-01-29 20:18:05 -0500

    Edit 1b of set_permissions.sh

--- set-permissions.sh
+++ set-permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chown -R "bhatts39:bhatts39" "bhatts39/CS1XA3"
 chmod -R 755 "bhatts39/CS1XA3/public_html"
-chmod -R 755 "bhatts39/CS1XA3/private"
-chmod 755 README.md
+chmod -R 770 "bhatts39/CS1XA3/private"
+chmod 644 README.md
