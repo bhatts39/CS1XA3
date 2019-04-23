442e21602c5a07785dbc5b6d9923526193d3e3e4
Sunny <bhatts39@mcmaster.ca>
2019-04-23 23:03:00 +0000

    Created game and trivia models

--- Project03/django_project/trivia/models.py
+++ Project03/django_project/trivia/models.py
@@ -4,20 +4,56 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class UserInfo(models.Model):
-	user = models.OneToOneField(User,
+    user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
                                 primary_key=True,
                                 related_name='info')
-	points = models.IntegerField(default=0,max_length=10)
-	
+    points = models.IntegerField(default=0)
+
+class TriviaQuestion(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.TextField()
+    question = models.TextField()
+    choice1 = models.TextField()
+    choice2 = models.TextField()
+    choice3 = models.TextField()
+    choice4 = models.TextField()
+    correctChoice = models.IntegerField()
+
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    status = models.IntegerField(default=0)
+    p1 = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name='p1')
+    p2 = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name='p2',
+                                default=None,
+                                null=True
+                                )
+    p1lives = models.IntegerField(default=2)
+    p2lives = models.IntegerField(default=2)
+    p1choice = models.IntegerField(default=0)
+    p2choice = models.IntegerField(default=0)
+    questionNum = models.IntegerField(default=0)
+    question = models.ForeignKey(TriviaQuestion,
+                                on_delete=models.CASCADE,
+                                related_name='triviaQuestion')
+    timeUpdated = models.DateTimeField(auto_now=True)
+    winner = models.IntegerField(default=0)
+
+
+
 @receiver(post_save, sender=User)
-def create_user_infp(sender, instance, created, **kwargs):
+def create_user_info(sender, instance, created, **kwargs):
     if created:
         UserInfo.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_info(sender, instance, **kwargs):
     instance.info.save()
-    
+
+  
 
                             
--- Project03/django_project/trivia/views.py
+++ Project03/django_project/trivia/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.db.models import Q
 from django.template.loader import get_template
 from django.contrib.auth import *
 from django.contrib.auth.models import User
+from trivia.models import *
+import requests, time, json, random
 
 def login_view(request):
     if request.method == "POST":
@@ -12,7 +15,7 @@ def login_view(request):
     elif request.method == "GET":
         username = request.GET['username']
         password = request.GET['password']
-        user = authenticate(requzest, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return HttpResponse("success")
@@ -28,10 +31,10 @@ def home_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponse("logged out")
-    
+
 def register_view(request):
     if request.method == "POST":
-        t = get_template('login.html')
+        t = get_template('lsogin.html')
         html = t.render()
         return HttpResponse(html)
     elif request.method == "GET":
@@ -43,18 +46,54 @@ def register_view(request):
         user.save()
         return HttpResponse("success")
         
-def register_view(request):
-    if request.method == "POST":
-        t = get_template('login.html')
-        html = t.render()
-        return HttpResponse(html)
-    elif request.method == "GET":
-        username = request.GET['username']
-        password = request.GET['password']
-        email = request.GET['email']
-        user = User.objects.create_user(username, email, password)
-        user.info.score = 0
-        user.save()
-        return HttpResponse("success")
-        
+def findgame_view(request):
+    if(request.user.is_authenticated):
+        game = Game.objects.filter((Q(status=0) | Q(status=1)) & Q(p1=request.user) | Q(p2=request.user)).first()
+        if game is not None:
+            return JsonResponse({'error':'already in a game'})
+        game = Game.objects.filter(status=0).first()
+        if game == None:
+            question = newTriviaQuestion()
+            game = Game(status=0,p1=request.user,question=question)
+            game.save()
+            return JsonResponse({'status':'0','gameid':game.id})
+        else:
+            game.p2 = request.user
+            game.status = 1
+            game.save()
+            return JsonResponse({'status':'1','gameid':game.id})
+            
+
+def gameinfo_view(request):
+    if request.method == "GET" and request.user.is_authenticated:
+        game = Game.objects.filter(Q(id=request.GET['gameid']) & Q(p1=request.user) | Q(p2=request.user)).first()
+        if game is not None:
+            player = 'p1' if game.p1 == request.user else 'p2'
+            p2 = '' if game.p2 == None else game.p2.id
+            return JsonResponse({'status':game.status,
+                                'player':player,
+                                'p1':game.p1.id,
+                                'p2':p2,
+                                'p1choice':game.p1choice,
+                                'p2choice':game.p2choice,
+                                'question':game.question.id,
+                                'questionNum':game.questionNum}) 
+        else:
+            return JsonResponse({'error':'game not found'})
+def newTriviaQuestion():
+    response = requests.get("https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple")
+    data = json.loads(response.text).get('results')[0]
+    question = data.get('question')
+    correctChoice = random.randint(0,3)
+    answers = data.get('incorrect_answers')
+    answers.insert(correctChoice,data.get('correct_answer'))
+    q = TriviaQuestion(question=question,
+                        category=data.get('category'),
+                        choice1=answers[0],
+                        choice2=answers[1],
+                        choice3=answers[2],
+                        choice4=answers[3],
+                        correctChoice=correctChoice)
+    q.save() 
+    return q
         
